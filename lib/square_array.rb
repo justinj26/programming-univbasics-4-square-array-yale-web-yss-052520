4ce431efa144b008a4dfe691f4640e7e089159aa
Justin James <justin.james@yale.edu>
2020-05-05 05:57:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,9 @@ def square_array(array)
   counter = 0
   new_array = []
   while array[counter] do 
-    new_array.push(array[counter]**2)
+    exponent = array[counter]**2 
+    new_array.push(exponent)
+    counter += 1 
   end
   new_array
 end
